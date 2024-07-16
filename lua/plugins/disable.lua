aa21a12def81ae8f00a92bf0dee47bb2d0c6a998
wenli7363 <cjy0415@qq.com>
2024-07-16 22:01:52 +0800

    init

---
+++ lua/plugins/disable.lua
@@ -0,0 +1,9 @@
+return {
+  { "folke/noice.nvim", enabled = false },
+  { "rcarriga/nvim-notify", enabled = false },
+  {
+    "stevearc/dressing.nvim",
+    enabled = false,
+  },
+  { "folke/trouble.nvim", enabled = false },
+}
